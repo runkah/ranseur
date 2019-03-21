f80580701896be96d12e4419205717b225a47b36
Irsay Collinz <knight@chinamail.com>
2019-03-21 11:20:00 -0400

    about us python

---
+++ aboutUS.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# aboutUS
+#print()runkahranseur
+def intro(intro_string):
+        if len(intro_string) % 2 != 0:
+            intro_string = intro_string + " "
+            
+        space = int((36 - len(intro_string)) / 2) * " "
+
+        tds = "%%%"
+
+        long_line = "\t" + "%" * 42 + "\n"
+
+        mid_line = "\t" + tds + (" " * 36) + tds + "\n"
+
+        title_line = "\t%s%s%s%s%s\n"  % (tds, space, intro_string, space, tds)
+
+        showme = long_line + mid_line + title_line + mid_line + long_line
+        print(showme)
+
+
+intro("RUNKAH")
+
+AoO=[]
+
+bio = ""
